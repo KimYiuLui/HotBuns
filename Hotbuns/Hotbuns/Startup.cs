7f678e4f48b05e5f8e6cffe83afbe80b41f451eb
KimYiuLui <kimyiu-lui@hotmail.com>
2018-09-27 22:34:57 +0200

    First commit
    Co-Authored-By: ImreBaas <imrebaas@users.noreply.github.com>

---
+++ Hotbuns/Hotbuns/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotbuns.Startup))]
+namespace Hotbuns
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
